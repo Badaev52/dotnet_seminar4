a8a5e0e42ed394e88969587d5a476a607f769f1f
badaev52 <badaev52@gmail.com>
2022-12-04 23:55:59 +0300

    end

---
+++ exercise29/Program.cs
@@ -0,0 +1,45 @@
+﻿int a = inputInt("размер массива");
+
+int[] array = new int[a];
+newArray(array);
+printArray(array);
+
+int inputInt(string name)
+{
+    Console.WriteLine("Введите число " + name + ": ");
+    bool isInt = int.TryParse(Console.ReadLine(), out int num);
+    if (isInt)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное число!");
+        return -1;
+    }
+}
+
+void newArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = inputInt("массива " + i);
+    }
+
+}
+
+
+void printArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (i == 0) Console.Write("[ ");
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(", ");
+        if (i == array.Length - 1) Console.Write(" ]");
+    }
+
+}
