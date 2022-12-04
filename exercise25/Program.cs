97caddbfc6a334e476f8cc1adc6db61eacd86f63
badaev52 <badaev52@gmail.com>
2022-12-04 23:29:28 +0300

    решение задач 25, 27

--- exercise25/Program.cs
+++ exercise25/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int a = inputInt("A");
+int b = inputInt("B");
+
+Console.WriteLine("Результат равен: " + intPow(a, b));
+
+
+
+int inputInt(string name)
+{
+    Console.WriteLine("Введите число " + name + ": ");
+    bool isInt = int.TryParse(Console.ReadLine(), out int num);
+    if (isInt)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное число!");
+        return -1;
+    }
+}
+
+int intPow(int a, int b)
+{
+    int res = a;
+    if (b == 0 ) return 1;
+    if (b == 1 ) return a;
+    for (int i = 2; i <= b; i++)
+    {
+        res = res * a;
+
+
+    }
+
+    return res;
+}
---
+++ exercise27/Program.cs
@@ -0,0 +1,46 @@
+﻿int num = inputInt();
+
+int dig = numberOfDigits(num);
+
+Console.WriteLine("Сумма цифр в числе равна: " + sumDigitsNumber(num, dig));
+
+
+int inputInt()
+{
+    Console.WriteLine("Введите число: ");
+    bool isInt = int.TryParse(Console.ReadLine(), out int num);
+    if (isInt)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное число!");
+        return -1;
+    }
+}
+
+int numberOfDigits(int number)
+{
+    int digits = 0;
+    if (number == 0) return 1;
+    while (number > 0)
+    {
+        number = number / 10;
+        digits++;
+    }
+    return digits;
+}
+
+int sumDigitsNumber(int num, int digits)
+{
+    int res = 0;
+
+    for (int i = 0; i < digits; i++)
+    {
+        res = res + num % 10;
+        num = num / 10;
+    }
+
+    return res;
+}
